c8f40eaf941f9978e575980e20c24cd7e3eff9ec
MohammedAnsal <ansalshaah786@gmail.com>
2024-08-05 00:05:24 +0530

    connect db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(() => {
+    console.log("MongoDB Connection Failed");
+  });
+
+const app = express();
 
 app.listen(5005, () => {
-    
-    console.log('Server Running');
-
+  console.log("Server Running");
 });
