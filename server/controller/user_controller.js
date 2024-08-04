a9ad97e61ef773838f88944bc7c773ec60d865da
MohammedAnsal <ansalshaah786@gmail.com>
2024-08-05 00:42:33 +0530

    create first api

---
+++ server/controller/user_controller.js
@@ -0,0 +1,15 @@
+
+export const loadSignup = (req, res) => {
+    
+    try {
+
+        console.log("Hello World");
+        
+    } catch (error) {
+
+        console.log(error.message);
+        
+        
+    }
+
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import user_route from "./routes/user_route.js";
 
 dotenv.config();
+
+const app = express();
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -13,7 +16,7 @@ mongoose
     console.log("MongoDB Connection Failed");
   });
 
-const app = express();
+app.use('/', user_route);
 
 app.listen(5005, () => {
   console.log("Server Running");
